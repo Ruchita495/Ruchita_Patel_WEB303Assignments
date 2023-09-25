a4f5520a68275631491e73c0a58d0b8a8413170e
Ruchita495 <122808258+Ruchita495@users.noreply.github.com>
2023-09-24 22:46:31 -0400

    add code

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// WEB303 Assignment 2
+var content = $('#content');
+
+$('a[href="#"]').on('click',function(){
+    var xhr = new XMLHttpRequest;
+    console.log(xhr);
+
+    var page = $(this).attr("id");
+    xhr.open('GET',page+'.html',true);
+    console.log(xhr);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            var contentHTML = xhr.responseText;
+            display(contentHTML);
+        }
+    }
+    xhr.send();
+})
+
+function display(contentHTML){
+    content.css('display','block');
+    content.html(contentHTML);
+}
