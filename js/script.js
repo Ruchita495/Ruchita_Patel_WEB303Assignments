f46f1ab92fc1e76258f8806075ee3c33c14d45b5
Ruchita495 <122808258+Ruchita495@users.noreply.github.com>
2023-10-01 17:36:55 -0400

    add file

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    displayAJAX();
+});
+
+function displayJSON(){
+    $.getJSON("team.json",function(data){
+        $.each(data.member,function(index,obj){
+            $("#team").append("<h2>"+ obj.name + "</h2> <h5>" + obj.position + "</h5> <p>" + obj.bio + "</p>");
+        });
+    });
+}
+
+function displayAJAX(){
+    $.ajax({
+        type:"GET",
+        url:"team.json",
+        dataType:"Json",
+        beforSend: function(){
+            $("#team").append("<h3> Loading....</h3>");
+        },
+
+        sucess : function(data){
+            setTimeout(function(){
+                $("#team").empty();
+                $.each(data.members,function(index,obj){
+                    $("#team").append("<h2>" + obj.name + "</h2> <h5>" + obj.position + "</h5> <p>" + obj.bio + "</p>");
+                });
+            }, 3000);
+        },
+
+        error : function(){
+            $("#team").append("<h3> Some error Occured... </h3>");
+        }
+    });
+}
